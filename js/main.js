08ba2092bd358e948314f6c97c47906cca71b4e1
Kirill <79596523+Melkorich@users.noreply.github.com>
2022-07-19 22:41:45 +0300

    Add files via upload
    animation added

--- js/main.js
+++ js/main.js
@@ -2,20 +2,23 @@
 
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu-mobile');
+const overlay = document.querySelector('.overlay');
+const navLinks = document.querySelectorAll('.menu-mobile__list a');
+
 
 menuBtn.addEventListener('click', function () {
     this.classList.toggle('menu-btn--active');
     menu.classList.toggle('menu-mobile--active');
     document.querySelector('body').classList.toggle('no-scroll');
+    overlay.classList.toggle('overlay--active');
 });
-
-const navLinks = document.querySelectorAll('.menu-mobile__list a');
 
 navLinks.forEach(function (item) {
     item.addEventListener('click', function () {
         menuBtn.classList.remove('menu-btn--active');
         menu.classList.remove('menu-mobile--active');
         document.querySelector('body').classList.remove('no-scroll');
+        overlay.classList.remove('overlay--active');
     })
 })
 
